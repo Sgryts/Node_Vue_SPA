d6546a6e7947d13173449c1ec2301ead0c950a46
Sergii <sergiigrytsaienko@gmail.com>
2020-09-09 20:24:25 -0500

    added pagination to clients gallery

--- client/src/app/client/portfolio/components/portfolio.component.ts
+++ client/src/app/client/portfolio/components/portfolio.component.ts
@@ -6,7 +6,9 @@ import {
   OnDestroy,
   OnInit,
   Output,
-  SecurityContext
+  SecurityContext,
+  SimpleChanges,
+  OnChanges
 } from '@angular/core';
 import { IAlbum } from 'ngx-lightbox/lightbox-event.service';
 import IGenre from '../../../models/genre.model';
@@ -18,13 +20,36 @@ import { Lightbox } from 'ngx-lightbox';
   styleUrls: ['./portfolio.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnChanges {
   @Input() photos: IAlbum[];
   @Input() genres: IGenre[];
   @Input() isLoading: boolean;
   @Output() selectedGenre = new EventEmitter<string>();
 
+  public data = [];
+  public data2 = [];
+  page = 0;
+  size = 4;
+
   constructor(private lightbox: Lightbox) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes?.photos) {
+      this.data2 = this.photos;
+      this.setPaging({ pageIndex: this.page, pageSize: this.size });
+    }
+  }
+
+
+  public setPaging(data: { pageIndex: number; pageSize: number; }): void {
+    let index = 0,
+      startingIndex = data.pageIndex * data.pageSize,
+      endingIndex = startingIndex + data.pageSize;
+    this.data = this.data2.filter((): boolean => {
+      index++;
+      return (index > startingIndex && index <= endingIndex);
+    });
   }
 
   onGenreSelected(id: string) {
