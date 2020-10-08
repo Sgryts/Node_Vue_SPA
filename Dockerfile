394cb25c5398140c757b675b1dc9234ce0f2368f
Sergii <sergiigrytsaienko@gmail.com>
2020-10-07 20:18:59 -0500

    1. Supporting cluster restarts. 2. Docker & npm commands update

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,20 @@
-FROM node:14 as client-build
+FROM node:14 AS client-build
+RUN apt-get update
+RUN npm cache clean --force
+RUN npm i -g npm
+RUN npm install -g gulp-cli
 WORKDIR /usr/src/app
-COPY client .
-RUN npm install && npm run build
+COPY client/ ./client
+COPY server/ ./server
+RUN cd /usr/src/app/client && npm install
+RUN cd /usr/src/app/server && npm install && gulp
 
-FROM node:14 as server-build
+FROM node:14 AS server-build
 WORKDIR /root/
-COPY --from=client-build /usr/src/app/dist ./dist
-COPY package*.json ./
-RUN npm install 
-COPY . .
+COPY --from=client-build /usr/src/app/prod-build .
+# COPY package*.json ./
+RUN npm install --production
+# COPY . .
 
 EXPOSE 3000
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -16,18 +16,37 @@ const PORT: string = CONFIG.PORT;
 const INSECURE_PORT = CONFIG.INSECURE_PORT;
 const SECURE_PORT = CONFIG.SECURE_PORT;
 const HOST: string = CONFIG.HOST;
-const options: { [key: string]: Buffer } = {
-  key: fs.readFileSync('src/cert/server.key'),
-  cert: fs.readFileSync('src/cert/server.crt')
-};
+// const options: { [key: string]: Buffer } = {
+//   key: fs.readFileSync('src/cert/server.key'),
+//   cert: fs.readFileSync('src/cert/server.crt')
+// };
 
 if (cluster.isMaster) {
   for (let i = 0; i < os.cpus().length; i++) {
     cluster.fork();
-
-    cluster.on('online', worker => console.log(colors.rainbow('Worker ' + worker.process.pid + 'is online')));
-    cluster.on('exit', (worker, code, signal) => console.log(colors.rainbow('Worker ' + worker.process.pid + 'exited')));
   }
+
+  cluster.on('online', worker => console.log(colors.rainbow(`Worker ${worker.process.pid} is online`)));
+  cluster.on('exit', (worker, code, signal) => {
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log(colors.red(`Worker ${worker.process.pid} crashed. Starting a new worker`));
+      cluster.fork();
+    }
+  });
+  process.on('SIGUSR2', () => {
+    const workers = Object.values(cluster.workers);
+    const restartWorker = (workerIndex) => {
+      const worker = workers[workerIndex];
+      if (!worker) return;
+      worker.on('exit', () => {
+        if (!worker.exitedAfterDisconnect) return;
+        cluster.fork();
+      });
+      worker.disconnect();
+    }
+    restartWorker(0);
+  });
+
 } else {
   app.listen(PORT, (err: any) => {
     if (err) {
