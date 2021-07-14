c2b0a00fafb35f3837663cfc67d3ea32d3d09959
Sergii <sergiigrytsaienko@gmail.com>
2021-07-13 19:45:12 -0500

    Client: minor fixes for the photos upload form

--- client/src/app/admin/photos/components/upload/photos-upload.component.ts
+++ client/src/app/admin/photos/components/upload/photos-upload.component.ts
@@ -1,11 +1,11 @@
 import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  ViewChild
+    ChangeDetectionStrategy,
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    ViewChild
 } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { IDropdownSettings } from 'ng-multiselect-dropdown/multiselect.model';
@@ -13,161 +13,161 @@ import IGenre from '../../../../models/genre.model';
 import { IPhotoUpload } from '../../photo-upload.model';
 
 @Component({
-  selector: 'admin-photos-upload',
-  templateUrl: './photos-upload.component.html',
-  styleUrls: ['./photos-upload.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'admin-photos-upload',
+    templateUrl: './photos-upload.component.html',
+    styleUrls: ['./photos-upload.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PhotosUploadComponent implements OnInit {
-  @Input() genres: IGenre[];
-  @Input() completed: boolean;
-  @Input() progress: number;
-  @Input() error: string;
-  @Input() isInProgress: boolean;
-  @Input() isReady: boolean;
-  @Input() hasFailed: boolean;
+    @Input() genres: IGenre[];
+    @Input() completed: boolean;
+    @Input() progress: number;
+    @Input() error: string;
+    @Input() isInProgress: boolean;
+    @Input() isReady: boolean;
+    @Input() hasFailed: boolean;
 
-  @Output() photoUploaded: EventEmitter<IPhotoUpload> = new EventEmitter<IPhotoUpload>();
-  @Output() photoUploadedReset: EventEmitter<void> = new EventEmitter<void>();
-  @Output() photoUploadedCanceled: EventEmitter<void> = new EventEmitter<void>();
+    @Output() photoUploaded: EventEmitter<IPhotoUpload> = new EventEmitter<IPhotoUpload>();
+    @Output() photoUploadedReset: EventEmitter<void> = new EventEmitter<void>();
+    @Output() photoUploadedCanceled: EventEmitter<void> = new EventEmitter<void>();
 
-  public form: FormGroup;
-  private file: File | null;
+    public form: FormGroup;
+    private file: File | null;
 
-  @ViewChild('multiSelect') multiSelect;
-  public loadContent: boolean = false;
-  public name: string = 'Genres Select Dropdown';
-  public placeholder: string = 'Select Genre(s)';
-  public data: IGenre[] = [];
-  public settings: IDropdownSettings = {};
-  // public selectedItems = [];
+    @ViewChild('multiSelect') multiSelect;
+    public loadContent: boolean = false;
+    public name: string = 'Genres Select Dropdown';
+    public placeholder: string = 'Select Genre(s)';
+    public data: IGenre[] = [];
+    public settings: IDropdownSettings = {};
+    // public selectedItems = [];
 
-  constructor(private fb: FormBuilder) {
-  }
-
-  ngOnInit(): void {
-    this.settings = {
-      singleSelection: false,
-      idField: '_id',
-      textField: 'name',
-      enableCheckAll: true,
-      selectAllText: 'Select All',
-      unSelectAllText: 'Unselect All',
-      allowSearchFilter: true,
-      limitSelection: -1,
-      clearSearchFilter: true,
-      maxHeight: 250,
-      itemsShowLimit: 5,
-      searchPlaceholderText: 'Search Genre',
-      noDataAvailablePlaceholderText: 'No Genres Available',
-      closeDropDownOnSelection: false,
-      showSelectedItemsAtTop: false,
-      defaultOpen: false
-    };
-    this.setForm();
-  }
-
-  public setForm() {
-    this.form = this.fb.group({
-      name: [null,
-        [Validators.required, Validators.minLength(1),
-        Validators.maxLength(255)]],
-      genres: [null,
-        [Validators.required, this.genresValidator()]],
-      image: [null,
-        [Validators.required]]
-    });
-    this.loadContent = true;
-  }
-
-  get f(): { [key: string]: AbstractControl; } {
-    return this.form.controls;
-  }
-
-  public save(): void {
-    this.photoUploaded.emit({
-      name: this.form.get('name').value,
-      file: this.file,
-      genres: this.form.get('genres').value.map((v) => v._id)
-    });
-    this.resetForm();
-  }
-
-  public resetForm() {
-    this.setForm();
-    this.multiSelect.toggleSelectAll();
-  }
-
-  // APIs:
-  public onFilterChange(item: any) {
-    console.log(item);
-  }
-
-  public onDropDownClose(item: any) {
-    console.log(item);
-  }
-
-  public onItemSelect(item: any) {
-    console.log(item);
-  }
-
-  public onDeSelect(item: any) {
-    console.log(item);
-  }
-
-  public onSelectAll(items: any) {
-    console.log(items);
-  }
-
-  public onDeSelectAll(items: any) {
-    console.log(items);
-  }
-
-  public uploadPhoto(event: any): void {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.file = file;
+    constructor(private fb: FormBuilder) {
     }
-    event.srcElement.value = null;
-  }
 
-  public resetPhotoUpload(): void {
-    this.resetForm();
-    this.photoUploadedReset.emit();
-  }
+    ngOnInit(): void {
+        this.settings = {
+            singleSelection: false,
+            idField: '_id',
+            textField: 'name',
+            enableCheckAll: true,
+            selectAllText: 'Select All',
+            unSelectAllText: 'Unselect All',
+            allowSearchFilter: true,
+            limitSelection: -1,
+            clearSearchFilter: true,
+            maxHeight: 250,
+            itemsShowLimit: 5,
+            searchPlaceholderText: 'Search Genre',
+            noDataAvailablePlaceholderText: 'No Genres Available',
+            closeDropDownOnSelection: false,
+            showSelectedItemsAtTop: false,
+            defaultOpen: false
+        };
+        this.setForm();
+    }
 
-  public cancelPhotoUpload(): void {
-    this.resetForm();
-    this.photoUploadedCanceled.emit();
-  }
+    public setForm() {
+        this.form = this.fb.group({
+            name: [null,
+                [Validators.required, Validators.minLength(1),
+                Validators.maxLength(255)]],
+            genres: [null,
+                [Validators.required, this.genresValidator()]],
+            image: [null,
+                [Validators.required]]
+        });
+        this.loadContent = true;
+    }
 
-  private genresValidator() {
-    return (c: FormControl) => {
-      if (c?.value) {
-        if (c.value.some((v: IGenre): boolean =>
-          v?._id.length !== 24)) {
-          return {
-            genresInvalid: true
-          };
+    get f(): { [key: string]: AbstractControl; } {
+        return this.form.controls;
+    }
+
+    public save(): void {
+        this.photoUploaded.emit({
+            name: this.form.get('name').value,
+            file: this.file,
+            genres: this.form.get('genres').value.map((v: IGenre): string => v._id)
+        });
+        this.resetForm();
+    }
+
+    public resetForm() {
+        this.setForm();
+        this.multiSelect.toggleSelectAll();
+    }
+
+    // APIs:
+    public onFilterChange(item: any) {
+        console.log(item);
+    }
+
+    public onDropDownClose(item: any) {
+        console.log(item);
+    }
+
+    public onItemSelect(item: any) {
+        console.log(item);
+    }
+
+    public onDeSelect(item: any) {
+        console.log(item);
+    }
+
+    public onSelectAll(items: any) {
+        console.log(items);
+    }
+
+    public onDeSelectAll(items: any) {
+        console.log(items);
+    }
+
+    public uploadPhoto(event: any): void {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            this.file = file;
         }
-      }
-      return null;
-    };
-  }
+        event.srcElement.value = null;
+    }
 
-  private fileTypeValidator(types: string[]) {
-    return (c: FormControl) => {
-      const file = c.value;
-      if (file) {
-        const extension = file.name.split('.')[1].toLocaleLowerCase();
-        if (!types.includes(extension)) {
-          return {
-            fileTypeInvalid: true
-          };
-        }
-        return null;
-      }
-      return null;
-    };
-  }
+    public resetPhotoUpload(): void {
+        this.resetForm();
+        this.photoUploadedReset.emit();
+    }
+
+    public cancelPhotoUpload(): void {
+        this.resetForm();
+        this.photoUploadedCanceled.emit();
+    }
+
+    private genresValidator() {
+        return (c: FormControl) => {
+            if (c?.value) {
+                if (c.value.some((v: IGenre): boolean =>
+                    v?._id.length !== 24)) {
+                    return {
+                        genresInvalid: true
+                    };
+                }
+            }
+            return null;
+        };
+    }
+
+    private fileTypeValidator(types: string[]) {
+        return (c: FormControl) => {
+            const file = c.value;
+            if (file) {
+                const extension = file.name.split('.')[1].toLocaleLowerCase();
+                if (!types.includes(extension)) {
+                    return {
+                        fileTypeInvalid: true
+                    };
+                }
+                return null;
+            }
+            return null;
+        };
+    }
 }
