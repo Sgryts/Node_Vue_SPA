5a8ec92ed34c12444ab3b88faeffac2a46cad448
Sergii <sergiigrytsaienko@gmail.com>
2021-09-22 22:39:46 -0500

    Server: Modules&Controllers added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PhotosModule } from './photos/photos.module';
+import { GenresModule } from './genres/genres.module';
+import { AuthModule } from './auth/auth.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [],
+  imports: [PhotosModule, GenresModule, AuthModule, ContactModule],
   controllers: [AppController],
   providers: [AppService],
 })
